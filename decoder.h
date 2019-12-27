0e86b8e222caa807b64c051131fec8f3cbae3e53
Mocktezuma <adrian.salas@protonmail.com>
2019-12-27 12:06:05 +0100

    added stop loop for stop codon

--- decoder.h
+++ decoder.h
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
-
-std::string codon; //codon to decode
-
-std::string stop[3] = {"uaa","uag","uga"}; //stop codon
+//codon to decode
+string stop[3] = {"uaa","uag","uga"}; //stop codon
 
 string decoder(string codon)
     {
-    for(int i = 0; i<=3; ++i) 
+    for(int i = 0; i<=2; ++i) 
         {
-        if (codon=stop[i])
+        if (codon==stop[i]){
             codon = "stop";
-            return codon;
+            cout << "your codon is :" << codon << endl;
         }
-    return 0;
+        }
+    return codon;
     }
--- main.cpp
+++ main.cpp
@@ -3,20 +3,20 @@
 #include "decoder.h"
 using namespace std;
 
+string codon;
 
-int interface(string codon){
+int interface(){
     cout << "Enter a codon: " << endl;
     cin >> codon;
-    codon = decoder(codon);
+    decoder(codon);
     cout << "your codon is " << codon << endl;
-
     return 0;
 }
 
 
 
 int main(){
-    interface(codon);
+    interface();
 
     return 0;
 
