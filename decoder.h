b9a0ebf587730aa4c10df6721c8cea70a798e87e
Mocktezuma <adrian.salas@protonmail.com>
2019-12-28 13:45:52 +0100

    why cant i do this with c++....

--- decoder.h
+++ decoder.h
@@ -5,16 +5,16 @@ using namespace std;
 //codon to decode
 string stop[3] = {"uaa","uag","uga"}; //stop codon
 
-string codonOneA[4] = {"ag","au","aa","ac"};
-string codonOneG[4] = {"ga","gc","gu","gg"};
-string codonOneC[4] = {"ca","cc","cu","cg"};
-string codonOneU[4] = {"ua","uc","uu","ug"};
+string codonOne = "guac";
+
 
 string decoder(string codon)
     {
     for(int i = 0; i<=2; ++i) // loop that looks for stop codon before looking for anything else
         if (codon==stop[i])
             codon = "stop";
-    
+    if(codon[0] == "a")
+        if(codon[1]==codonOne[2])
+            codon = "Threonine";
     return codon;
     }
