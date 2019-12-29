3c585513c620cd45fc31f0da35107f4f897cd285
Mocktezuma <adrian.salas@protonmail.com>
2019-12-29 11:09:22 +0100

    added codon starting with C

--- decoder.h
+++ decoder.h
@@ -13,8 +13,22 @@ string decoder(string codon)
     for(int i = 0; i<=2; ++i) // loop that looks for stop codon before looking for anything else
         if (codon==stop[i])
             codon = "stop";
-    if(codon[0] == codonOne[2])
+    
+    if(codon[0] == codonOne[2]) // evaluation for codon starting with A
         if(codon[1]==codonOne[3])
             codon = "Threonine";
+    
+    if(codon[0] == codonOne[0]) // evaluation for codon starting with G
+
+    if(codon[0] == codonOne[1]) // evaluation for codon starting with U
+    
+    if(codon[0] == codonOne[3]) // evaluation for codon starting with C 
+        if(codon[1]==codonOne[0])
+            codon = "Arginine";
+        if(codon[1]==codonOne[1])
+            codon = "Leucine";
+        if(codon[1]==codonOne[3])
+            codon = "Proline";
+    
     return codon;
     }
