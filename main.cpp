ef896df911e74851f513d69bed4aefeb00fbf4a1
Mocktezuma <adrian.salas@protonmail.com>
2019-12-29 11:27:14 +0100

    added comments and error message + fixed shit

--- main.cpp
+++ main.cpp
@@ -8,8 +8,16 @@ string codon;
 void interface(){
     cout << "Enter a codon: " << endl;
     cin >> codon;
-    codon = decoder(codon);
-    cout << "your codon is : " << codon << endl;
+    if (codon.length()==3)
+    {
+        codon = decoder(codon); // function that decodes codon into an amino acid from a sequence of G,U,A,C characters.
+        cout << "your codon is : " << codon << endl; // return result from codon as an amino acid or stop.
+    }
+    else
+    {
+        cout << "Error: your codon must be 3 characters long." << endl; // return error when codon is not 3 characters long.
+    }
+    
 }
 
 
