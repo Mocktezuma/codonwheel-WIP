5b51260d6e05ae92ef21edc9f1ea46b95f14ed9b
Mocktezuma <adrian.salas@protonmail.com>
2020-01-07 15:38:36 +0100

    i suck at c++ trying switch instead of if else

--- decoder.h
+++ decoder.h
@@ -11,24 +11,22 @@ string codonOne = "guac";
 string decoder(string codon)
     {
     for(int i = 0; i<=2; ++i) // loop that looks for stop codon before looking for anything else
-        if (codon==stop[i])
-            codon = "stop";
+        {if (codon==stop[i])
+            {codon = "stop";}
+        } 
+    switch(codon[0] == codonOne[2]) // evaluation for codon starting with A
+        {if(codon[1]==codonOne[3])
+            codon = "Threonine";}
     
-    if(codon[0] == codonOne[2]) // evaluation for codon starting with A
-        if(codon[1]==codonOne[3])
-            codon = "Threonine";
-    
-    if(codon[0] == codonOne[0]) // evaluation for codon starting with G
-
-    if(codon[0] == codonOne[1]) // evaluation for codon starting with U
-    
-    if(codon[0] == codonOne[3]) // evaluation for codon starting with C 
-        if(codon[1]==codonOne[0])
-            codon = "Arginine";
-        if(codon[1]==codonOne[1])
-            codon = "Leucine";
-        if(codon[1]==codonOne[3])
-            codon = "Proline";
-                
-    return codon;
-    }
+    switch(codon[0] == codonOne[0]) // evaluation for codon starting with G
+        {}
+    switch(codon[0] == codonOne[1]) // evaluation for codon starting with U
+    {}
+    switch(codon[0] == codonOne[3]){ // evaluation for codon starting with C 
+        if(codon[1]==codonOne[0]){
+            codon = "Arginine";}
+        else if(codon[1]==codonOne[1]){
+            codon = "Leucine";}
+        else if(codon[1]==codonOne[3]){
+            codon = "Proline";}}
+    return codon;}
